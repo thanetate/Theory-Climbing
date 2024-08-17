1964508e9521205ce33001340be2ee52ab3a761c
Thane Tate <thanetate@my.unt.edu>
2024-08-17 14:37:59 -0500

    order page UI

--- my-app/app/(front)/order/[id]/OrderDetails.tsx
+++ my-app/app/(front)/order/[id]/OrderDetails.tsx
@@ -79,132 +79,136 @@ export default function OrderDetails({
 
   return (
     <div>
-      <h1 className="text-2xl py-4">Order {orderId}</h1>
-      <div className="grid md:grid-cols-4 md:gap-5 my-4">
-        <div className="md:col-span-3">
-          <div className="card bg-base-300">
-            <div className="card-body">
-              <h2 className="card-title">Shipping Address</h2>
-              <p>{shippingAddress.fullName}</p>
-              <p>
-                {shippingAddress.address}, {shippingAddress.city},{" "}
-                {shippingAddress.postalCode}, {shippingAddress.country}{" "}
-              </p>
-              {isDelivered ? (
-                <div className="text-success">Delivered at {deliveredAt}</div>
-              ) : (
-                <div className="text-error">Not Delivered</div>
-              )}
-            </div>
-          </div>
+      <div className="page-container">
+        <div className="place-order-container">
+          <h1 className="order-title">Order #{orderId}</h1>
+          <div className="">
+            <div className="">
+              <div className="">
+                <div className="pcard-body">
+                  <h2 className="p-title">Shipping Address</h2>
+                  <p className="p">{shippingAddress.fullName}</p>
+                  <p className="p">
+                    {shippingAddress.address}, {shippingAddress.city},{" "}
+                    {shippingAddress.postalCode}, {shippingAddress.country}{" "}
+                  </p>
+                  {isDelivered ? (
+                    <div className="p">Status: Delivered at {deliveredAt}</div>
+                  ) : (
+                    <div className="p">Status: Not Delivered</div>
+                  )}
+                </div>
+              </div>
 
-          <div className="card bg-base-300 mt-4">
-            <div className="card-body">
-              <h2 className="card-title">Payment Method</h2>
-              <p>{paymentMethod}</p>
-              {isPaid ? (
-                <div className="text-success">Paid at {paidAt}</div>
-              ) : (
-                <div className="text-error">Not Paid</div>
-              )}
-            </div>
-          </div>
+              <div className="payment-method-container">
+                <div className="pmcard-body">
+                  <h2 className="pm-title">Payment Method</h2>
+                  <p className="p">{paymentMethod}</p>
+                  {isPaid ? (
+                    <div className="p">Status: Paid at {paidAt}</div>
+                  ) : (
+                    <div className="p">Status: Not Paid</div>
+                  )}
+                </div>
+              </div>
 
-          <div className="card bg-base-300 mt-4">
-            <div className="card-body">
-              <h2 className="card-title">Items</h2>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>Item</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {items.map((item: OrderItem) => (
-                    <tr key={item.slug}>
-                      <td>
-                        <Link
-                          href={`/product/${item.slug}`}
-                          className="flex items-center"
+              <div className="cartp-container">
+                <div className="cart-body">
+                  <h2 className="cart-title">Items</h2>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th className="cartp-i">Item</th>
+                        <th className="cartp-q">Quantity</th>
+                        <th className="cartp-p">Price</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {items.map((item: OrderItem) => (
+                        <tr key={item.slug}>
+                          <td>
+                            <Link
+                              href={`/product/${item.slug}`}
+                              className="flex items-center"
+                            >
+                              <Image
+                                src={item.image}
+                                alt={item.name}
+                                width={50}
+                                height={50}
+                              ></Image>
+                              <span className="px-2">
+                                {item.name} ({item.color} {item.size})
+                              </span>
+                            </Link>
+                          </td>
+                          <td className="i-q">{item.qty}</td>
+                          <td className="i-p">${item.price}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="summary-container">
+                <div className="summary-body">
+                  <h2 className="s-title">Order Summary</h2>
+                  <ul className="s-list">
+                    <li>
+                      <div className="s-items">
+                        <div>Items:</div>
+                        <div>${itemsPrice}</div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="s-tax">
+                        <div>Tax:</div>
+                        <div>${taxPrice}</div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="s-shipping">
+                        <div>Shipping:</div>
+                        <div>${shippingPrice}</div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="s-total">
+                        <div>Total:</div>
+                        <div>${totalPrice}</div>
+                      </div>
+                    </li>
+
+                    {!isPaid && paymentMethod === "PayPal" && (
+                      <li>
+                        <PayPalScriptProvider
+                          options={{ clientId: paypalClientId }}
                         >
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            width={50}
-                            height={50}
-                          ></Image>
-                          <span className="px-2">
-                            {item.name} ({item.color} {item.size})
-                          </span>
-                        </Link>
-                      </td>
-                      <td>{item.qty}</td>
-                      <td>${item.price}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div className="card bg-base-300">
-            <div className="card-body">
-              <h2 className="card-title">Order Summary</h2>
-              <ul>
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Items</div>
-                    <div>${itemsPrice}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Tax</div>
-                    <div>${taxPrice}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Shipping</div>
-                    <div>${shippingPrice}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Total</div>
-                    <div>${totalPrice}</div>
-                  </div>
-                </li>
-
-                {!isPaid && paymentMethod === "PayPal" && (
-                  <li>
-                    <PayPalScriptProvider
-                      options={{ clientId: paypalClientId }}
-                    >
-                      <PayPalButtons
-                        createOrder={createPayPalOrder}
-                        onApprove={onApprovePayPalOrder}
-                      />
-                    </PayPalScriptProvider>
-                  </li>
-                )}
-                {session?.user.isAdmin && (
-                  <li>
-                    <button
-                      className="btn w-full my-2"
-                      onClick={() => deliverOrder()}
-                      disabled={isDelivering}
-                    >
-                      {isDelivering && <span className="spinner"></span>}
-                      Mark as delivered
-                    </button>
-                  </li>
-                )}
-              </ul>
+                          <PayPalButtons
+                            createOrder={createPayPalOrder}
+                            onApprove={onApprovePayPalOrder}
+                          />
+                        </PayPalScriptProvider>
+                      </li>
+                    )}
+                    {session?.user.isAdmin && (
+                      <li>
+                        <button
+                          className="btn w-full my-2"
+                          onClick={() => deliverOrder()}
+                          disabled={isDelivering}
+                        >
+                          {isDelivering && <span className="spinner"></span>}
+                          Mark as delivered
+                        </button>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
