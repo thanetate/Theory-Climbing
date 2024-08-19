49e0e94354c277589d8c4a1a772ccf2bd676b90d
Thane Tate <thanetate@my.unt.edu>
2024-08-19 18:26:02 -0500

    fix build error

--- my-app/app/api/orders/mine/route.ts
+++ my-app/app/api/orders/mine/route.ts
@@ -1,3 +1,4 @@
+/*
 import dbConnect from "@/lib/dbConnect";
 import OrderModel from "@/lib/models/OrderModel";
 import { auth } from "@/lib/auth";
@@ -16,3 +17,24 @@ export const GET = auth(async (req: any) => {
   const orders = await OrderModel.find({ user: user._id });
   return Response.json(orders);
 });
+*/
+import dbConnect from "@/lib/dbConnect";
+import OrderModel from "@/lib/models/OrderModel";
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export const GET = auth(async (req: Request & { auth: any }) => {
+  // Update type of req
+  if (!req.auth) {
+    return NextResponse.json(
+      { message: "unauthorized" },
+      {
+        status: 401,
+      }
+    );
+  }
+  const { user } = req.auth;
+  await dbConnect();
+  const orders = await OrderModel.find({ user: user._id });
+  return NextResponse.json(orders);
+}) as (req: Request) => Promise<Response>; // Explicitly set return type
