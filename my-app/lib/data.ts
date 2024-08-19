80147e8ee507d9a8c0b40fffde379b74d13abeb8
Thane Tate <thanetate@my.unt.edu>
2024-08-19 18:51:46 -0500

    data.ts

---
+++ my-app/lib/data.ts
@@ -0,0 +1,50 @@
+import bcrypt from "bcryptjs";
+
+const data = {
+  users: [
+    {
+      name: "Thane Tate",
+      email: "thanetate1@gmail.com",
+      password: bcrypt.hashSync("Pooh5791"),
+      isAdmin: true,
+    },
+    {
+      name: "Bailee Gasche",
+      email: "bailee.gasche@icloud.com",
+      password: bcrypt.hashSync("Family5791"),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      name: "COMING SOON",
+      slug: "product-1",
+      category: "Shirts",
+      image: "/images/fashion-1.jpeg",
+      price: 1,
+      brand: "theory",
+      rating: 5,
+      numReviews: 0,
+      countInStock: 100,
+      description: "Product Coming Soon...",
+      isFeatured: true,
+      banner: "", //image
+    },
+    {
+      name: "COMING SOON",
+      slug: "product-2",
+      category: "Shirts",
+      image: "/images/fashion-2.jpeg",
+      price: 1,
+      brand: "theory",
+      rating: 5,
+      numReviews: 0,
+      countInStock: 100,
+      description: "Product Coming Soon...",
+      isFeatured: true,
+      banner: "", //image
+    },
+  ],
+};
+
+export default data;
