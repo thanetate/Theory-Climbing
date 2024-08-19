111c61d21b507cb57df40344d4ede7ef309cc0cd
Thane Tate <thanetate@my.unt.edu>
2024-08-19 16:35:20 -0500

    bugs

--- my-app/app/(front)/signin/Form.tsx
+++ my-app/app/(front)/signin/Form.tsx
@@ -1,21 +1,21 @@
-'use client'
-import { signIn, useSession } from 'next-auth/react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
-import Link from 'next/link'
+"use client";
+import { signIn, useSession } from "next-auth/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import Link from "next/link";
 
 type Inputs = {
-  email: string
-  password: string
-}
+  email: string;
+  password: string;
+};
 
 const Form = () => {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
 
-  const params = useSearchParams()
-  let callbackUrl = params.get('callbackUrl') || '/'
-  const router = useRouter()
+  const params = useSearchParams();
+  let callbackUrl = params.get("callbackUrl") || "/";
+  const router = useRouter();
 
   const {
     register,
@@ -23,37 +23,37 @@ const Form = () => {
     formState: { errors, isSubmitting },
   } = useForm<Inputs>({
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
-  })
+  });
 
   useEffect(() => {
     if (session && session.user) {
-      router.push(callbackUrl)
+      router.push(callbackUrl);
     }
-  }, [callbackUrl, params, router, session])
+  }, [callbackUrl, params, router, session]);
 
   const formSubmit: SubmitHandler<Inputs> = async (form) => {
-    const { email, password } = form
-    signIn('credentials', {
+    const { email, password } = form;
+    signIn("credentials", {
       email,
       password,
-    })
-  }
+    });
+  };
   return (
     <div className="card-container">
       <div className="profile-card">
         <h1 className="card-title">Sign in</h1>
-        {params.get('error') && (
-          <div className="alert text-error">
-            {params.get('error') === 'CredentialsSignin'
-              ? 'Invalid email or password'
-              : params.get('error')}
+        {params.get("error") && (
+          <div className="form-error">
+            {params.get("error") === "CredentialsSignin"
+              ? "Invalid email or password"
+              : params.get("error")}
           </div>
         )}
-        {params.get('success') && (
-          <div className="alert text-success">{params.get('success')}</div>
+        {params.get("success") && (
+          <div className="form-error">{params.get("success")}</div>
         )}
         <form onSubmit={handleSubmit(formSubmit)}>
           <div className="form-group">
@@ -63,11 +63,11 @@ const Form = () => {
             <input
               type="text"
               id="email"
-              {...register('email', {
-                required: 'Email is required',
+              {...register("email", {
+                required: "Email is required",
                 pattern: {
                   value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                  message: 'Email is invalid',
+                  message: "Email is invalid",
                 },
               })}
               className="form-input"
@@ -83,8 +83,8 @@ const Form = () => {
             <input
               type="password"
               id="password"
-              {...register('password', {
-                required: 'Password is required',
+              {...register("password", {
+                required: "Password is required",
               })}
               className="form-input"
             />
@@ -93,11 +93,7 @@ const Form = () => {
             )}
           </div>
           <div className="pbtn-container">
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className="p-btn"
-            >
+            <button type="submit" disabled={isSubmitting} className="p-btn">
               {isSubmitting && (
                 <span className="loading loading-spinner"></span>
               )}
@@ -106,13 +102,16 @@ const Form = () => {
           </div>
         </form>
         <div className="form-question">
-          Need an account?{' '}
-          <Link className="fq-link" href={`/register?callbackUrl=${callbackUrl}`}>
+          Need an account?{" "}
+          <Link
+            className="fq-link"
+            href={`/register?callbackUrl=${callbackUrl}`}
+          >
             Register
           </Link>
         </div>
       </div>
     </div>
-  )
-}
-export default Form
+  );
+};
+export default Form;
