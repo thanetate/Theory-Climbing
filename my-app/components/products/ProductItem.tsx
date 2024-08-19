b8ab39ce52a8ba98ea6bfc574b67c246e735cd48
Thane Tate <thanetate@my.unt.edu>
2024-08-19 12:12:31 -0500

    updating UI

--- my-app/components/products/ProductItem.tsx
+++ my-app/components/products/ProductItem.tsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import { Product } from '@/lib/models/ProductModel'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import { Product } from "@/lib/models/ProductModel";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function ProductItem({ product }: { product: Product }) {
-	return (
-        <div className="">
-            <figure>
-                <Link href={`/product/${product.slug}`}>
-                    <Image
-                        src={product.image}
-                        alt={product.name}
-                        width={300}
-                        height={300}
-                    />
-                </Link>
-            </figure>
-            <div className="product-desc">
-                <Link href={`/product/${product.slug}`}>
-                    <h2 className="product-name">{product.name}</h2>
-                </Link>
-                <p className="product-brand">{product.brand}</p>
-                <div className="product-price">
-                    ${product.price}
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="">
+      <figure>
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={300}
+            height={300}
+          />
+        </Link>
+      </figure>
+      <div className="product-desc">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="product-name">Coming Soon</h2>
+        </Link>
+        <p className="product-brand">{product.brand}</p>
+        <div className="product-price">$1</div>
+      </div>
+    </div>
+  );
 }
-
