33f5197400d35d1de429da953268871f6cb68b56
Thane Tate <thanetate@my.unt.edu>
2024-08-19 13:10:16 -0500

    working profile

--- my-app/lib/dbConnect.ts
+++ my-app/lib/dbConnect.ts
@@ -1,11 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 async function dbConnect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!)
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log("Successfully connected to MongoDB");
   } catch (error) {
-    throw new Error('Connection failed!')
+    if (error instanceof Error) {
+      console.error("MongoDB connection error:", error.message);
+      throw new Error(`Connection failed! ${error.message}`);
+    } else {
+      console.error("Unexpected error:", error);
+      throw new Error("Connection failed due to an unexpected error");
+    }
   }
 }
 
-export default dbConnect
+export default dbConnect;
