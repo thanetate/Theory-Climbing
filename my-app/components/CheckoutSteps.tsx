eb9ef8acd454b19a67ab35a2ad009621bea19a1b
Thane Tate <thanetate@my.unt.edu>
2024-09-01 11:39:38 -0500

    fix checkout steps UI

--- my-app/components/CheckoutSteps.tsx
+++ my-app/components/CheckoutSteps.tsx
@@ -1,18 +1,16 @@
 const CheckoutSteps = ({ current = 0 }) => {
   return (
     <ul className="steps steps-vertical lg:steps-horizontal w-full mt-4">
-      {["User Login", "Shipping", "Payment", "Place Order"].map(
-        (step, index) => (
-          <li
-            key={step}
-            className={`step
+      {["Login", "Shipping", "Payment", "Order"].map((step, index) => (
+        <li
+          key={step}
+          className={`step
            ${index <= current ? "step-primary" : ""}
            `}
-          >
-            {step}
-          </li>
-        )
-      )}
+        >
+          {step}
+        </li>
+      ))}
     </ul>
   );
 };
